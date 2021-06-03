2cfd5e3e38abd7813da2550398456b262f1246d2
joeyhofman123 <joeyhofman1@gmail.com>
2021-06-03 09:50:48 +0200

    woorden in totaal statistiek toegevoegd

---
+++ app/Models/TextWord.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TextWord extends Model
+{
+    use HasFactory;
+
+    protected $table = "text_word";
+}
